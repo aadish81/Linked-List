d70f0db08261fd18540bf97e7e59c2c59534ab2e
Aadish <adishjoshi1111@gmail.com>
2022-04-08 08:13:39 +0545

    function for printing and freeing memory has been added

--- linkedls.c
+++ linkedls.c
@@ -6,21 +6,24 @@ typedef struct node{
     struct node *next;
 }
 node;
+
+void printls(node *list);
+void reales_ls(node *list);
 int add_to_list(int element,node *root);
+
 int main(void){
 
     node *list = NULL;
-
     int result = add_to_list(5 ,list);
     
-
     if(result == 0){
-      printf("%i \n",list->number);
-
-      free(list);
+      printls(list);
+      reales_ls(list);
       return 0;
   }
-  return 0;
+  else{
+      return 1;
+  }
 
 }
 
@@ -42,4 +45,15 @@ int add_to_list(int element,node *root){
     }
     
   return 0;
+}
+void printls(node *list){
+    for(node *tem = list;tem != NULL;tem = tem->next){
+        printf("%i\n",tem->number);
+    }
+}
+
+void reales_ls(node *list){
+    for(node *tem = list;tem !=NULL;tem = tem->next){
+        free(tem);
+    }
 }
