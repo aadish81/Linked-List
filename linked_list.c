64646d3075d12783b6249ff5b1615d9f68800b4e
Aadish <adishjoshi1111@gmail.com>
2022-04-10 13:00:51 +0545

    first version of linked list is ready

--- linked_list.c
+++ linked_list.c
@@ -6,53 +6,53 @@ typedef struct node{
     struct node *next;
 }
 node;
-
-void printls(node *list);
-void reales_ls(node *list);
-int add_to_list(int element,node *root);
+node *list = NULL;
+int add_to_list(int element);
+void printls(void);
+void reales_ls(void);
 
 int main(void){
 
-    node *list = NULL;
-    int result = add_to_list(5 ,list);
-    
-    if(result == 0){
-      printls(list);
-      reales_ls(list);
-      return 0;
-  }
-  else{
+    int result = add_to_list(5689);
+
+    if(result != 0){
       return 1;
-  }
+    }
+    
+
+    printls();
+    reales_ls();
+    return 0;
+    
 
 }
 
-int add_to_list(int element,node *root){
+int add_to_list(int element){
     node *temp = malloc(sizeof(node));
     if(temp == NULL){
         free(temp);
         return 1;
     }
     temp->number = element;
-    if(root == NULL){
+    if(list == NULL){
         temp->next= NULL;
-        root = temp;
+        list = temp;
     }
     else{
-        temp->next = root;
-        root = temp;
-        free(temp);
+        temp->next = list;
+        list = temp;
+        
     }
-    
-  return 0;
+    return 0;
 }
-void printls(node *list){
+void printls(void){
     for(node *tem = list;tem != NULL;tem = tem->next){
         printf("%i\n",tem->number);
     }
+
 }
 
-void reales_ls(node *list){
+void reales_ls(void){
     for(node *tem = list;tem !=NULL;tem = tem->next){
         free(tem);
     }
