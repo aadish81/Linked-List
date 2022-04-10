991f906b1a34b2478270da76d830db1efcf3f058
Aadish <adishjoshi1111@gmail.com>
2022-04-10 14:14:55 +0545

    linked_list.c has been improved but still needs some

--- linked_list.c
+++ linked_list.c
@@ -6,20 +6,29 @@ typedef struct node{
     struct node *next;
 }
 node;
+
 node *list = NULL;
+int search(int a);
 int add_to_list(int element);
 void printls(void);
 void reales_ls(void);
 
 int main(void){
-
-    int result = add_to_list(5689);
-
-    if(result != 0){
-      return 1;
+    int num_of_elm;
+    int num;
+    int result;
+    printf("Enter the number of element you want to store: ");
+    scanf("%i",&num_of_elm);
+    for(int i = 0; i<num_of_elm;i++){
+        printf("Enter the number: ");
+        scanf("%i\n",&num);
+        result = add_to_list(num);
+        if(result != 0){
+            printf("System Error.");
+            reales_ls();
+            return 0;
+        }
     }
-    
-
     printls();
     reales_ls();
     return 0;
@@ -41,10 +50,10 @@ int add_to_list(int element){
     else{
         temp->next = list;
         list = temp;
-        
     }
     return 0;
 }
+
 void printls(void){
     for(node *tem = list;tem != NULL;tem = tem->next){
         printf("%i\n",tem->number);
@@ -55,5 +64,17 @@ void printls(void){
 void reales_ls(void){
     for(node *tem = list;tem !=NULL;tem = tem->next){
         free(tem);
+    }
+}
+
+
+int search(int a){
+    for(node *tem = list;tem != NULL;tem = tem->next){
+        if(tem->number == a){
+            printf("Found");
+            return 0;
+        }
+    printf("Not Found");
+    return 0;    
     }
 }
